7caaa70364dea07936ccc7ee21f70fd6dc46d0ff
Jungha <kidultree@naver.com>
2022-05-17 11:24:11 +0900

    First Project Commit "2022-05-17"

---
+++ Semi_Project_HKCLH/src/main/java/project/home/study/SemiProjectHkclhApplication.java
@@ -0,0 +1,13 @@
+package project.home.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectHkclhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectHkclhApplication.class, args);
+	}
+
+}
