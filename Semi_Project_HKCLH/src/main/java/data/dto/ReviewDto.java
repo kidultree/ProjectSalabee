3bc422596df93a9c1951adfa426bc0412d3e91aa
Jungha <kidultree@naver.com>
2022-05-26 18:27:08 +0900

    Review ㅁH끵

---
+++ Semi_Project_HKCLH/src/main/java/data/dto/ReviewDto.java
@@ -0,0 +1,23 @@
+package data.dto;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Alias("review")
+@Data
+public class ReviewDto {
+
+	private int rnum;
+	private int rrate;
+	private int rtitle;
+	private String rname;
+	private String rcontent;
+	private String rphoto;
+	private Timestamp rdate;
+	
+	private int pnum;
+	private String mid;
+}
